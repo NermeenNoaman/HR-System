581552df549bbd9336a535c97e0a3e9b8601b080
NermeenNoaman <167822087+NermeenNoaman@users.noreply.github.com>
2025-12-07 18:35:01 +0200

    Update IAttendanceRepository.cs

--- HRSystem.Infrastructure/Contracts/IAttendanceRepository.cs
+++ HRSystem.Infrastructure/Contracts/IAttendanceRepository.cs
@@ -1,4 +1,4 @@
-﻿using HRSystem.BaseLibrary.Models;
+using HRSystem.BaseLibrary.Models;
 using System.Threading.Tasks;
 
 namespace HRSystem.Infrastructure.Contracts
@@ -10,7 +10,7 @@ namespace HRSystem.Infrastructure.Contracts
 
         // 2. Check-Out: Update check-out time and hours
         // We use AttendanceID to specify the record to be updated
-        Task<bool> RecordCheckOutAsync(int attendanceId, DateTime checkOutTime, string status = "Present");
+        Task<bool> RecordCheckOutAsync(int attendanceId, TimeSpan checkOutTime, string status = "Present");
 
         // 3. Reporting/Validation: Retrieve attendance records for an employee within a specific time range.
         Task<IEnumerable<TPLAttendance>> GetAttendanceByDateRangeAsync(int employeeId, DateTime startDate, DateTime endDate);
